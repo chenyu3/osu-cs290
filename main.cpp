3800dfcca2d980d84d09693f3c68c1ce4eb70b04
chenyu3 <32278143+chenyu3@users.noreply.github.com>
2017-09-30 20:53:54 -0700

    Add files via upload
    Add second file for assignment1 completion.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Eunice on 9/30/17.
+//  Copyright © 2017 Eunice. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+    return 0;
+}
